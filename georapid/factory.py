563e830645857e432da5a8a4e47036e5f590ab28
gisfromscratch <gisfromscratch@live.de>
2022-08-13 13:55:45 +0200

    Added a new factory function

--- georapid/factory.py
+++ georapid/factory.py
@@ -25,7 +25,7 @@ class EnvironmentClientFactory(object):
     @staticmethod
     def create_client():
         """
-        Creates a new client using 'x_rapidapi_url', 'x_rapidapi_host' and 'x_rapidapi_key'.
+        Creates a new client using 'x_rapidapi_url', 'x_rapidapi_host' and 'x_rapidapi_key' environment variables.
         Raises a ValueError when these variables are not defined!
         """
         if not 'x_rapidapi_url' in os.environ:
@@ -37,6 +37,23 @@ class EnvironmentClientFactory(object):
 
         url = os.environ['x_rapidapi_url']
         host = os.environ['x_rapidapi_host']
+        key = os.environ['x_rapidapi_key']
+        auth_headers = {
+            'x-rapidapi-host': host,
+            'x-rapidapi-key': key
+        }
+
+        return GeoRapidClient(url, auth_headers)
+
+    @staticmethod
+    def create_client_with_host(url, host):
+        """
+        Creates a new client using 'x_rapidapi_key' enironment variable.
+        Raises a ValueError when this variable is not defined!
+        """
+        if not 'x_rapidapi_key' in os.environ:
+            raise ValueError("'x_rapidapi_key' is not defined in the current environment!")
+
         key = os.environ['x_rapidapi_key']
         auth_headers = {
             'x-rapidapi-host': host,
